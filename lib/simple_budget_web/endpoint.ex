1b233dfff9621bd8a6f56ec8c8bbeac518ea4540
Cory Buecker <cory.buecker@gmail.com>
2018-10-21 12:44:46 -0500

    gzip assets

--- lib/simple_budget_web/endpoint.ex
+++ lib/simple_budget_web/endpoint.ex
@@ -12,7 +12,7 @@ defmodule SimpleBudgetWeb.Endpoint do
   plug Plug.Static,
     at: "/",
     from: :simple_budget,
-    gzip: false,
+    gzip: true,
     only: ~w(css fonts images js favicon.ico robots.txt)
 
   # Code reloading can be explicitly enabled under the

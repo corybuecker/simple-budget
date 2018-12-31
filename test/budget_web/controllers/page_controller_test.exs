b20f4cdbcef9afc0cc97bdb590f2ed51cb7fc930
Cory Buecker <email@corybuecker.com>
2018-12-31 05:35:48 -0600

    Add tests (#17)

--- test/budget_web/controllers/page_controller_test.exs
+++ test/budget_web/controllers/page_controller_test.exs
@@ -6,6 +6,21 @@ defmodule SimpleBudgetWeb.PageControllerTest do
     assert html_response(conn, 200) =~ "app\.js"
   end
 
+  test "GET /accounts", %{conn: conn} do
+    conn = get(conn |> init_test_session(%{token: "validid"}), "/accounts")
+    assert html_response(conn, 200) =~ "accounts\.js"
+  end
+
+  test "GET /goals", %{conn: conn} do
+    conn = get(conn |> init_test_session(%{token: "validid"}), "/goals")
+    assert html_response(conn, 200) =~ "goals\.js"
+  end
+
+  test "GET /savings", %{conn: conn} do
+    conn = get(conn |> init_test_session(%{token: "validid"}), "/savings")
+    assert html_response(conn, 200) =~ "savings\.js"
+  end
+
   test "unauthorized GET /", %{conn: conn} do
     conn = get(conn, "/")
     assert redirected_to(conn, 302) =~ "/login"

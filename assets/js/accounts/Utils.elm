fb30483db5e25adec831c55edf9fc5170184de1d
Cory Buecker <email@corybuecker.com>
2019-01-01 06:46:09 -0600

    Remove `exposing` and `as` per Elm guidelines (#22)

--- assets/js/accounts/Utils.elm
+++ assets/js/accounts/Utils.elm
@@ -1,19 +1,19 @@
 module Accounts.Utils exposing (accountDecoder, accountUpdatedDecoder, accountsDecoder, adjustmentDecoder, balance, encode, id, name)
 
-import Accounts.Models exposing (Account, Adjustment)
+import Accounts.Models
 import Json.Decode
 import Json.Decode.Pipeline
-import Json.Encode as Encode exposing (Value, int, object, string)
+import Json.Encode
 
 
-accountsDecoder : Json.Decode.Decoder (List Account)
+accountsDecoder : Json.Decode.Decoder (List Accounts.Models.Account)
 accountsDecoder =
     Json.Decode.field "data" (Json.Decode.list accountDecoder)
 
 
-accountDecoder : Json.Decode.Decoder Account
+accountDecoder : Json.Decode.Decoder Accounts.Models.Account
 accountDecoder =
-    Json.Decode.succeed Account
+    Json.Decode.succeed Accounts.Models.Account
         |> Json.Decode.Pipeline.required "id" Json.Decode.int
         |> Json.Decode.Pipeline.required "name" Json.Decode.string
         |> Json.Decode.Pipeline.required "debt" Json.Decode.bool
@@ -21,40 +21,40 @@ accountDecoder =
         |> Json.Decode.Pipeline.required "adjustments" (Json.Decode.list adjustmentDecoder)
 
 
-accountUpdatedDecoder : Json.Decode.Decoder Account
+accountUpdatedDecoder : Json.Decode.Decoder Accounts.Models.Account
 accountUpdatedDecoder =
     Json.Decode.field "data" accountDecoder
 
 
-adjustmentDecoder : Json.Decode.Decoder Adjustment
+adjustmentDecoder : Json.Decode.Decoder Accounts.Models.Adjustment
 adjustmentDecoder =
-    Json.Decode.succeed Adjustment
+    Json.Decode.succeed Accounts.Models.Adjustment
         |> Json.Decode.Pipeline.required "account_id" Json.Decode.int
         |> Json.Decode.Pipeline.required "id" Json.Decode.int
         |> Json.Decode.Pipeline.required "title" Json.Decode.string
         |> Json.Decode.Pipeline.required "total" Json.Decode.float
 
 
-id : Int -> ( String, Encode.Value )
+id : Int -> ( String, Json.Encode.Value )
 id value =
-    ( "id", Encode.int value )
+    ( "id", Json.Encode.int value )
 
 
-name : String -> ( String, Encode.Value )
+name : String -> ( String, Json.Encode.Value )
 name value =
-    ( "name", Encode.string value )
+    ( "name", Json.Encode.string value )
 
 
-balance : Float -> ( String, Encode.Value )
+balance : Float -> ( String, Json.Encode.Value )
 balance value =
-    ( "balance", Encode.float value )
+    ( "balance", Json.Encode.float value )
 
 
-debt : Bool -> ( String, Encode.Value )
+debt : Bool -> ( String, Json.Encode.Value )
 debt value =
-    ( "debt", Encode.bool value )
+    ( "debt", Json.Encode.bool value )
 
 
-encode : Account -> Encode.Value
+encode : Accounts.Models.Account -> Json.Encode.Value
 encode schema =
-    Encode.object [ ( "account", Encode.object [ id schema.id, name schema.name, debt schema.debt, balance schema.balance ] ) ]
+    Json.Encode.object [ ( "account", Json.Encode.object [ id schema.id, name schema.name, debt schema.debt, balance schema.balance ] ) ]

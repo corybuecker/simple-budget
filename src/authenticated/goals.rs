67c692f265d48c8d30a686aa9db786086c513dd5
Cory Buecker <email@corybuecker.com>
2024-06-23 11:42:41 -0500

    add routes

--- src/authenticated/goals.rs
+++ src/authenticated/goals.rs
@@ -1,7 +1,18 @@
 use crate::SharedState;
 use axum::{routing::get, Router};
+mod create;
+mod delete;
+mod edit;
 mod index;
+mod new;
+mod update;
 
 pub fn goals_router() -> Router<SharedState> {
-    Router::new().route("/", get(index::index))
+    Router::new()
+        .route("/", get(index::page).post(create::page))
+        .route(
+            "/:id",
+            get(edit::page).put(update::page).delete(delete::page),
+        )
+        .route("/new", get(new::page))
 }

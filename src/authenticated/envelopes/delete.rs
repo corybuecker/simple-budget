83d428641c1301e25ac19f99e122abc2a8dad2ea
Cory Buecker <email@corybuecker.com>
2024-07-06 09:59:08 -0500

    fix envelope deletion
    closes #47

--- src/authenticated/envelopes/delete.rs
+++ src/authenticated/envelopes/delete.rs
@@ -1,18 +1,47 @@
 use crate::{authenticated::UserExtension, SharedState};
 use axum::{
-    extract::State,
+    extract::{Path, State},
     http::StatusCode,
-    response::{Html, IntoResponse, Response},
+    response::{Html, IntoResponse, Redirect, Response},
     Extension,
 };
+use bson::doc;
+use bson::oid::ObjectId;
+use bson::serde_helpers::hex_string_as_object_id;
+use core::str::FromStr;
+use serde::{Deserialize, Serialize};
 use tera::Context;
 
-pub async fn page(shared_state: State<SharedState>, user: Extension<UserExtension>) -> Response {
+#[derive(Serialize, Deserialize, Debug)]
+pub struct EnvelopeRecord {
+    name: String,
+    amount: f64,
+    #[serde(with = "hex_string_as_object_id")]
+    user_id: String,
+}
+
+pub async fn page(
+    shared_state: State<SharedState>,
+    user: Extension<UserExtension>,
+    Path(id): Path<String>,
+) -> Result<Response, StatusCode> {
     log::debug!("{:?}", user);
-    let context = Context::new();
-    let Ok(content) = shared_state.tera.render("envelopes/index.html", &context) else {
-        return StatusCode::INTERNAL_SERVER_ERROR.into_response();
+    let envelopes: mongodb::Collection<EnvelopeRecord> = shared_state
+        .mongo
+        .database("simple_budget")
+        .collection("envelopes");
+
+    let filter = doc! {"_id": ObjectId::from_str(&id).unwrap(), "user_id": ObjectId::from_str(&user.id).unwrap()};
+
+    let Ok(envelope) = envelopes.find_one(filter.clone()).await else {
+        return Err(StatusCode::INTERNAL_SERVER_ERROR);
     };
 
-    Html::from(content).into_response()
+    let Some(_) = envelope else {
+        return Err(StatusCode::NOT_FOUND);
+    };
+
+    let _ = envelopes.delete_one(filter).await;
+
+    Ok(Redirect::to("/envelopes").into_response())
 }

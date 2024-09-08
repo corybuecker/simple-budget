75cab6d659f65ec3af364f560fd43ef79efd23ef
Cory Buecker <email@corybuecker.com>
2024-09-08 17:42:08 -0500

    add delete modal for accounts (#167)

--- src/authenticated/accounts/delete.rs
+++ src/authenticated/accounts/delete.rs
@@ -4,12 +4,42 @@ use crate::{
 use axum::{
     extract::{Path, State},
     http::StatusCode,
-    response::{Html, IntoResponse, Redirect, Response},
+    response::{Html, IntoResponse, Response},
     Extension,
 };
 use mongodb::bson::{doc, oid::ObjectId};
 use std::str::FromStr;
+use tera::Context;
 use tracing::info;
+
+pub async fn modal(
+    shared_state: State<SharedState>,
+    user: Extension<UserExtension>,
+    Path(id): Path<String>,
+) -> Result<Response, FormError> {
+    let accounts: mongodb::Collection<Account> = shared_state
+        .mongo
+        .default_database()
+        .unwrap()
+        .collection("accounts");
+
+    let filter = doc! {"_id": ObjectId::from_str(&id).unwrap(), "user_id": ObjectId::from_str(&user.id).unwrap()};
+
+    let account = accounts.find_one(filter.clone()).await?;
+
+    let Some(_) = account else {
+        return Err(FormError {
+            message: "could not find account".to_string(),
+            status_code: Some(StatusCode::NOT_FOUND),
+        });
+    };
+    let tera = shared_state.tera.clone();
+    let mut context = Context::new();
+    context.insert("account", &account);
+    let content = tera.render("accounts/delete/confirm.html", &context)?;
+
+    Ok(Html::from(content).into_response())
+}
 
 pub async fn action(
     shared_state: State<SharedState>,
@@ -31,19 +61,28 @@ pub async fn action(
     };
     let _ = accounts.delete_one(filter).await?;
 
-    Ok(Redirect::to("/accounts").into_response())
+    let tera = shared_state.tera.clone();
+    let mut context = Context::new();
+    context.insert("account", &account);
+    let content = tera.render("accounts/delete.html", &context)?;
+
+    Ok((
+        StatusCode::OK,
+        [("content-type", "text/vnd.turbo-stream.html")],
+        Html::from(content),
+    )
+        .into_response())
 }
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::models::account::Account;
-    
+
     use crate::test_utils::test_utils::{state_for_tests, user_for_tests};
     use axum::body::Body;
     use axum::http::Request;
     use axum::Router;
-    
-    
+
     use tower::ServiceExt;
 
     #[tokio::test]
@@ -78,7 +117,7 @@ mod tests {
 
         let response = app.oneshot(request).await.unwrap();
 
-        assert_eq!(response.status(), StatusCode::SEE_OTHER);
+        assert_eq!(response.status(), StatusCode::OK);
 
         let deleted_account = accounts.find_one(doc! {"_id": account_id}).await.unwrap();
         assert!(deleted_account.is_none());

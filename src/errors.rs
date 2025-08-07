a57b01031f54d9a80f86a5ad419cd68f71d021e8
Cory Buecker <email@corybuecker.com>
2025-08-06 19:21:57 -0500

    feat: add template-specific error handling (#452)

--- src/errors.rs
+++ src/errors.rs
@@ -1,3 +1,5 @@
+use std::error::Error;
+
 use axum::{
     http::StatusCode,
     response::{IntoResponse, Response},
@@ -8,15 +10,83 @@ use tracing::error;
 pub enum AppError {
     RecordNotFound(tokio_postgres::Error),
     RecordDeserializationError(tokio_postgres::Error),
+    TemplateError(tera::Error),
     Unknown(anyhow::Error),
 }
 
-impl<E> From<E> for AppError
-where
-    E: Into<anyhow::Error>,
-{
-    fn from(value: E) -> Self {
+impl From<tera::Error> for AppError {
+    fn from(value: tera::Error) -> Self {
+        Self::TemplateError(value)
+    }
+}
+
+impl From<tokio_postgres::Error> for AppError {
+    fn from(value: tokio_postgres::Error) -> Self {
         Self::Unknown(value.into())
+    }
+}
+
+impl From<serde_json::Error> for AppError {
+    fn from(value: serde_json::Error) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl From<deadpool_postgres::PoolError> for AppError {
+    fn from(value: deadpool_postgres::PoolError) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl From<jsonwebtoken::errors::Error> for AppError {
+    fn from(value: jsonwebtoken::errors::Error) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl From<chrono_tz::ParseError> for AppError {
+    fn from(value: chrono_tz::ParseError) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl From<openidconnect::url::ParseError> for AppError {
+    fn from(value: openidconnect::url::ParseError) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl From<openidconnect::reqwest::Error> for AppError {
+    fn from(value: openidconnect::reqwest::Error) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl
+    From<
+        openidconnect::DiscoveryError<
+            openidconnect::HttpClientError<openidconnect::reqwest::Error>,
+        >,
+    > for AppError
+{
+    fn from(
+        value: openidconnect::DiscoveryError<
+            openidconnect::HttpClientError<openidconnect::reqwest::Error>,
+        >,
+    ) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl From<std::env::VarError> for AppError {
+    fn from(value: std::env::VarError) -> Self {
+        Self::Unknown(value.into())
+    }
+}
+
+impl From<anyhow::Error> for AppError {
+    fn from(value: anyhow::Error) -> Self {
+        Self::Unknown(value)
     }
 }
 
@@ -29,8 +99,9 @@ impl IntoResponse for AppError {
                 error!("{}", err);
                 StatusCode::NOT_FOUND.into_response()
             }
-            AppError::Unknown(err) => {
-                error!("{}", err);
+            AppError::TemplateError(err) => {
+                error!("{:?} - {:?}", err.kind, err.source());
+
                 (
                     StatusCode::INTERNAL_SERVER_ERROR,
                     "Sorry, something has gone wrong",

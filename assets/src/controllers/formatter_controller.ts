fc98ae8ca7e030b2962f78d44cc2e67056c7da03
Cory Buecker <email@corybuecker.com>
2026-06-02 06:18:03 -0500

    chore: add TS typings

--- assets/src/controllers/formatter_controller.ts
+++ assets/src/controllers/formatter_controller.ts
@@ -1,14 +1,20 @@
 import { Controller } from '@hotwired/stimulus'
 
 export default class FormatterController extends Controller {
+  declare readonly currencyValue: number
+  declare readonly hasCurrencyValue: boolean
+  declare readonly currencyPrecisionValue: number
+  declare readonly hasCurrencyPrecisionValue: boolean
+
   static values = {
     currency: Number,
     currencyPrecision: Number
   }
 
+
   connect() {
     if(this.hasCurrencyValue) {
-      this.element.innerText = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: this.hasCurrencyPrecisionValue ? this.currencyPrecisionValue : 0 }).format(this.currencyValue)
+      this.element.textContent = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD', maximumFractionDigits: this.hasCurrencyPrecisionValue ? this.currencyPrecisionValue : 0 }).format(this.currencyValue)
     }
   }
 }
